da21c7b9709c693b4a18ce5cbd5544a9dfdba1ed
Russell Carey <russellwcarey@gmail.com>
2020-08-27 15:18:04 -0600

    updating popoutPageNav pointer function with more assertions

--- pages/popoutNav.js
+++ pages/popoutNav.js
@@ -6,32 +6,59 @@ var popoutPageNav = {
       .click('@hamburgerIcon')
       .waitForElementVisible('@believe_BelieveMain')
       .expect.element('@believe_BelieveMain').text.to.equal('Believe')
+      .waitForElementVisible('believe_FollowingJesus')
       .expect.element('@believe_FollowingJesus').text.to.equal('Following Jesus')
+      .waitForElementVisible('believe_TheBookofMormon')
       .expect.element('@believe_TheBookofMormon').text.to.equal('The Book of Mormon')
+      .waitForElementVisible('believe_TheBible')
       .expect.element('@believe_TheBible').text.to.equal('The Bible')
+      .waitForElementVisible('believe_FamilyIsForever')
       .expect.element('@believe_FamilyIsForever').text.to.equal('Family is Forever')
+      .waitForElementVisible('believe_PurposeOfLife')
       .expect.element('@believe_PurposeOfLife').text.to.equal('Purpose of Life')
+      .waitForElementVisible('believe_ComingCloser')
       .expect.element('@believe_ComingCloser').text.to.equal('Coming Closer to God')
+      .waitForElementVisible('believe_JesusChristChurch')
       .expect.element('@believe_JesusChristChurch').text.to.equal('Jesus Christ\'s Church')
+      .waitForElementVisible('believe_Forgiveness')
       .expect.element('@believe_Forgiveness').text.to.equal('Forgiveness')
+      .waitForElementVisible('believe_LifeAfterDeath')
       .expect.element('@believe_LifeAfterDeath').text.to.equal('Life After Death')
+      .waitForElementVisible('believe_Baptism')
       .expect.element('@believe_Baptism').text.to.equal('Baptism')
+      .waitForElementVisible('believe_Temples')
       .expect.element('@believe_Temples').text.to.equal('Temples')
+      .waitForElementVisible('believe_HealthyLiving')
       .expect.element('@believe_HealthyLiving').text.to.equal('Healthy Living')
+      .waitForElementVisible('belong_BelongMain')
       .expect.element('@belong_BelongMain').text.to.equal('Belong')
+      .waitForElementVisible('belong_WorshipServices')
       .expect.element('@belong_WorshipServices').text.to.equal('Worship Services')
+      .waitForElementVisible('belong_PeopleAndPrograms')
       .expect.element('@belong_PeopleAndPrograms').text.to.equal('People and Programs')
+      .waitForElementVisible('belong_Missionaries')
       .expect.element('@belong_Missionaries').text.to.equal('Missionaries')
+      .waitForElementVisible('become_BecomeMain')
       .expect.element('@become_BecomeMain').text.to.equal('Become')
+      .waitForElementVisible('become_ThisIsChurch')
       .expect.element('@become_ThisIsChurch').text.to.equal('This is Church')
+      .waitForElementVisible('become_LightTheWorld')
       .expect.element('@become_LightTheWorld').text.to.equal('Light the World')
+      .waitForElementVisible('become_HearHim')
       .expect.element('@become_HearHim').text.to.equal('Hear Him')
+      .waitForElementVisible('become_BecauseOfHim')
       .expect.element('@become_BecauseOfHim').text.to.equal('Because of Him')
+      .waitForElementVisible('contact_ContactMain')
       .expect.element('@contact_ContactMain').text.to.equal('Contact')
+      .waitForElementVisible('contact_FreeBofM')
       .expect.element('@contact_FreeBofM').text.to.equal('Free Book of Mormon')
+      .waitForElementVisible('contact_MeetTheMish')
       .expect.element('@contact_MeetTheMish').text.to.equal('Meet with Missionaries')
+      .waitForElementVisible('@contact_FindAChurch')
       .expect.element('@contact_FindAChurch').text.to.equal('Find a Church')
+      .waitForElementVisible('@contact_Email')
       .expect.element('@contact_Email').text.to.equal('Email');
+
     return this
   }
 }
@@ -45,10 +72,14 @@ module.exports = {
       locateStrategy: 'css selector'
     },
     believe_BelieveMain: {
+      //selector: '/html/body/header/nav/div/div[2]/div/div[2]/div[1]/ul/li[1]/a',
+      //locateStrategy: 'xpath' 
       selector: '.page-header-nav_container > ul:nth-child(1) > li:nth-child(1) > a:nth-child(1)',
       locateStrategy: 'css selector'
     },
     believe_FollowingJesus: {
+      //selector: '/html/body/header/nav/div/div[2]/div/div[2]/div[1]/ul/li[1]/ul/li[1]/a',
+      //locateStrategy: 'xpath'
       selector: '.page-header-nav_container > ul:nth-child(1) > li:nth-child(1) > ul:nth-child(2) > li:nth-child(1) > a:nth-child(1)',
       locateStrategy: 'css selector'
     },
